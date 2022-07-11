f5cd3edacc6eef79584182f45294a865c71c0f7c
Nathan Krasney <natankrasney@gmail.com>
2022-07-11 13:02:29 +0300

    snackbar section fiinished

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -5,36 +5,53 @@ import SimpleAccordion, {
 import IPost from "../src/types/IPost";
 import path from "path";
 import { getServerAbsoluteUrl } from "../src/utils/server/server-utils";
+import MuiSnackbar from "../src/components/gen-ui/MuiSnackbar";
+import { AlertColor } from "@mui/material";
 
 export async function getStaticProps() {
   // -- get posts
   let posts: IPost[] = [];
   const url = path.join(getServerAbsoluteUrl(), "/api/posts");
+  let sevirity : AlertColor;
+  let message : string = "";
 
   try {
     const response = await fetch(url);
     posts = await response.json();
+    sevirity = 'success'
   } catch (error) {
+    sevirity = 'error';
+    message = 'Fetch error'
     console.error(error);
   }
 
   return {
-    props: { posts }, // will be passed to the page component as props
+    props: { posts ,sevirity, message}, // will be passed to the page component as props
   };
 }
 
 interface IProps {
   posts: IPost[];
+  sevirity : AlertColor;
+  message : string;
 }
 
-const Blog: FC<IProps> = ({ posts }) => {
+const Blog: FC<IProps> = ({ posts ,sevirity, message}) => {
   const items: ISimpleAccordionItem[] = posts.map((post) => {
     return { summary: post.subject, details: post.body };
   });
 
+  
+
   return (
     <div>
       <h2>Posts</h2>
+      {(sevirity != 'success') ? <MuiSnackbar
+        isOpen={true}
+        durationMs={6000}
+        sevirity={sevirity}
+        message={message}/> : null}
+      
       <SimpleAccordion items={items} />
     </div>
   );
