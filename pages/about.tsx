fcea749c2f242bc84979ea6773b0cd622ba629a2
Nathan Krasney <natankrasney@gmail.com>
2022-07-19 10:02:44 +0300

    about

--- pages/about.tsx
+++ pages/about.tsx
@@ -2,10 +2,10 @@ import Link from "next/link";
 
 const About = () => {
   return (
-    <div>
-      
-      This is About page
-    </div>
+    <>
+      <h2>This is About page</h2>
+      <p>This page is waiting for your creativity, change this to suit your needs</p>
+    </>
   );
 };
 
